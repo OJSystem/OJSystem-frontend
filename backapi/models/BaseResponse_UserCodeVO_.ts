a976ef03f31541db2e73f5f90c53bfcb267013a0
zhengluqian <2661837382@qq.com>
2024-05-21 21:51:29 +0800

    OJ判题系统的管理页面

---
+++ backapi/models/BaseResponse_UserCodeVO_.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { UserCodeVO } from './UserCodeVO';
+
+export type BaseResponse_UserCodeVO_ = {
+    code?: number;
+    data?: UserCodeVO;
+    message?: string;
+};
+
